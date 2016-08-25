5bce8f43ed8b01aeb3d714fa4725c18a88abc42e
Sun <klsforever@gmail.com>
2016-08-25 15:53:54 +0800

    添加统一的错误处理

--- backend-api.js
+++ backend-api.js
@@ -3,35 +3,6 @@
 /**
  * 一个用于与后端交互的数据层, 即统一地调用后端接口.
  * 
- * 这样做的好处在于对前端架构做初略的(MVC)分层: 视图层(View) -> 业务层(Controller) -> 数据层(Model)
- * 视图层只需要关注调用什么业务, 业务层负责将数据从数据层中取出来, 数据层做最底层的后端交互
- * 如果是比较简单的业务, 可以只需要视图层和数据层, 此时视图层承担了业务层的事情.
- * 
- * 例如下面的 MessageList 就相当于一个视图, 可以看作 Backbone 式的 MVC 模式
- * 注意: 我省略了构造 backendApi 的代码
- * function MessageList() {
- *     this.backendApi.invoke('getMessageList').then(this.render.bind(this));
- * }
- * MessageList.prototype.render = function(result) {
- *     // 在这里解析数据渲染视图, 你可能会在这里使用前端模版库, 或者简单的拼字符串
- * };
- * 
- * 当然如果你想足够简单, 也可以完全使用 jQuery 式直接操作 DOM, 不抽象出视图
- * $(function() {
- *     function renderMessageList() {
- *         this.backendApi.invoke('getMessageList').then(function(result) {
- *             // 在这里解析数据渲染视图
- *         });
- *     }
- * 
- *     renderMessageList();
- * });
- * 
- * 有一个统一的数据层, 即统一了数据输入和输出, 可以方便地做下面的事情
- * 1. 方便统一处理一些公共的行为, 例如请求超时, 请求失败, 未授权等等
- * 2. 方便知道前后端都使用了哪些接口
- * 3. 方便扩展功能, 例如添加缓存层
- * 
  * @param apiConfig {object} 所有后端接口的配置
  */
 function BackendApi(apiConfig) {
